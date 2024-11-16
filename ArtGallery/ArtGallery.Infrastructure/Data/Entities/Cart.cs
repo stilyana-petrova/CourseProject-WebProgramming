eb8eac5cd5fe41181170375828188d615b293155
Stilyana Petrova <99167111+stilyana-petrova@users.noreply.github.com>
2024-11-16 14:30:25 +0200

    Add files via upload

---
+++ ArtGallery/ArtGallery.Infrastructure/Data/Entities/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtGallery.Infrastructure.Data.Entities
+{
+    public class Cart
+    {
+        public string Id { get; set; }
+       
+       // public string CartId { get; set; }
+
+       // public int Quantity { get; set; }
+
+      //  public System.DateTime DateCreated { get; set; }
+        
+        //[ForeignKey(nameof(Product))]
+        //public int ProductId { get; set; }
+        //public virtual Product Product { get; set; }
+
+        [ForeignKey(nameof(Order))]
+
+        public int OrderId { get; set; }
+        public virtual Order Order { get; set; }
+
+    }
+}
